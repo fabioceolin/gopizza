90f6709b931e187743c454c44f00e7bd5630e189
Fábio Ceolin <fabioceolin98@gmail.com>
2022-06-22 02:49:48 -0300

    Product header

---
+++ src/screens/Product/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Platform, TouchableOpacity } from "react-native";
+import {
+  Container,
+  Header,
+  Title,
+  DeleteLabel,
+  Upload,
+  PickImageButton,
+  Label,
+  InputGroup,
+  InputGroupHeader,
+  MaxCharacters,
+  Form,
+} from "./styles";
+
+export function Product() {
+  return (
+    <Container behavior={Platform.OS === "ios" ? "padding" : undefined}>
+      <Header>
+        <Title>Cadastrar</Title>
+        <TouchableOpacity>
+          <DeleteLabel>Deletar</DeleteLabel>
+        </TouchableOpacity>
+      </Header>
+    </Container>
+  );
+}
