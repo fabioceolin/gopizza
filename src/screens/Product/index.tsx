16f80e14554c5e1a52fb272a36e35a3c6948e999
Fábio Ceolin <fabioceolin98@gmail.com>
2022-08-05 14:30:47 -0300

    Product validation and creation

--- src/screens/Product/index.tsx
+++ src/screens/Product/index.tsx
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
-import { Platform, TouchableOpacity } from "react-native";
+import { Platform, TouchableOpacity, ScrollView, Alert } from "react-native";
 import * as ImagePicker from "expo-image-picker";
+import firestore from "@react-native-firebase/firestore";
+import storage from "@react-native-firebase/storage";
 
 import { ButtonBack } from "@components/ButtonBack";
 import { Photo } from "@components/Photo";
+import { InputPrice } from "@components/InputPrice";
+import { Input } from "@components/Input";
 import { Button } from "@components/Button";
 
 import {
@@ -22,6 +26,61 @@ import {
 
 export function Product() {
   const [image, setImage] = useState("");
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [priceSizeP, setPriceSizeP] = useState("");
+  const [priceSizeM, setPriceSizeM] = useState("");
+  const [priceSizeG, setPriceSizeG] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function handleAdd() {
+    if (!name.trim()) {
+      return Alert.alert("Cadastro", "Informe o nome da pizza.");
+    }
+    if (!description.trim()) {
+      return Alert.alert("Cadastro", "Informe a descrição da pizza.");
+    }
+    if (!image) {
+      return Alert.alert("Cadastro", "Selecione a imagem da pizza.");
+    }
+
+    if (!priceSizeP || !priceSizeM || !priceSizeM) {
+      return Alert.alert(
+        "Cadastro",
+        "Informe o preço de todos os tamanhos da pizza."
+      );
+    }
+
+    setIsLoading(true);
+
+    const fileName = new Date().getTime();
+    const reference = storage().ref(`/pizzas/${fileName}.png`);
+
+    await reference.putFile(image);
+    const photo_url = await reference.getDownloadURL();
+
+    await firestore()
+      .collection("pizzas")
+      .add({
+        name,
+        name_insensitive: name.toLowerCase().trim(),
+        description,
+        prices_sizes: {
+          p: priceSizeP,
+          m: priceSizeM,
+          g: priceSizeG,
+        },
+        photo_url,
+        photo_path: reference.fullPath,
+      })
+      .then(() => Alert.alert("Cadastro", "Pizza cadastrada com sucesso."))
+      .catch(
+        (error) => console.log(error),
+        Alert.alert("Cadastro", "Não foi possível cadastrar a pizza.")
+      )
+      .finally(() => setIsLoading(false));
+  }
+
   async function handlePickerImage() {
     const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
 
@@ -38,24 +97,72 @@ export function Product() {
   }
   return (
     <Container behavior={Platform.OS === "ios" ? "padding" : undefined}>
-      <Header>
-        <ButtonBack />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Header>
+          <ButtonBack />
 
-        <Title>Cadastrar</Title>
+          <Title>Cadastrar</Title>
 
-        <TouchableOpacity>
-          <DeleteLabel>Deletar</DeleteLabel>
-        </TouchableOpacity>
-      </Header>
+          <TouchableOpacity>
+            <DeleteLabel>Deletar</DeleteLabel>
+          </TouchableOpacity>
+        </Header>
 
-      <Upload>
-        <Photo uri={image} />
-        <PickImageButton
-          title="Carregar"
-          type="secondary"
-          onPress={handlePickerImage}
-        />
-      </Upload>
+        <Upload>
+          <Photo uri={image} />
+          <PickImageButton
+            title="Carregar"
+            type="secondary"
+            onPress={handlePickerImage}
+          />
+        </Upload>
+
+        <Form>
+          <InputGroup>
+            <Label>Nome</Label>
+            <Input onChangeText={setName} value={name} />
+          </InputGroup>
+
+          <InputGroup>
+            <InputGroupHeader>
+              <Label>Descrição</Label>
+              <MaxCharacters>0 de 60 caracteres</MaxCharacters>
+            </InputGroupHeader>
+            <Input
+              multiline
+              maxLength={60}
+              style={{ height: 80 }}
+              onChangeText={setDescription}
+              value={description}
+            />
+          </InputGroup>
+
+          <InputGroup>
+            <Label>Tamanhos e preços</Label>
+            <InputPrice
+              size="P"
+              onChangeText={setPriceSizeP}
+              value={priceSizeP}
+            />
+            <InputPrice
+              size="M"
+              onChangeText={setPriceSizeM}
+              value={priceSizeM}
+            />
+            <InputPrice
+              size="G"
+              onChangeText={setPriceSizeG}
+              value={priceSizeG}
+            />
+          </InputGroup>
+
+          <Button
+            title="Cadastar pizza"
+            isLoading={isLoading}
+            onPress={handleAdd}
+          />
+        </Form>
+      </ScrollView>
     </Container>
   );
 }
